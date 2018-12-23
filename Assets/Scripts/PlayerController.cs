9dc4013579e1a50eb3d202f0b7301c41fb3bbac8
IIzzaya <linxuan_0709@icloud.com>
2018-12-23 21:49:55 +0800

    新的角色移动和翻滚

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,16 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 5f;
     public float rotateSpeed = 5f;
+    public float shootingMoveMultiplier = 1 / 3f;
+    public float diveRollSpeedMultiplier = 1f;
+    public AnimationCurve diveRollSpeedCurve;
+    public float diveRollYPosMultiplier = 1f;
+    public AnimationCurve diveRollYPosCurve;
+
+    public bool isMoving = false;
+    public bool isShooting = false;
+    public bool isDiveRolling = false;
+    public bool enableDiveRoll = true;
 
     public Transform Unrotate;
     private Animator myAnimator;
@@ -18,7 +28,46 @@ public class PlayerController : MonoBehaviour {
         transform.rotation = lerpQuaternion;
     }
 
+    public void CalDiveRollMultiplier(float duration) {
+        diveRollSpeedMultiplier = diveRollSpeedCurve.Evaluate(duration);
+        diveRollYPosMultiplier = diveRollYPosCurve.Evaluate(duration);
+    }
+
+    void DiveRollStart() {
+        Debug.Log("Dive Roll Start");
+        enableDiveRoll = false;
+        isDiveRolling = true;
+        myAnimator.SetBool("triggerDiveRoll", isDiveRolling);
+    }
+
+    public void DiveRollEnd() {
+        Debug.Log("Dive Roll End");
+        enableDiveRoll = true;
+        isDiveRolling = false;
+    }
+
     private void Update() {
+
+        if (Input.GetButton("Fire1")) {
+            isShooting = true;
+            myAnimator.SetBool("isShooting", isShooting);
+
+        } else {
+            isShooting = false;
+            myAnimator.SetBool("isShooting", isShooting);
+        }
+
+        if (isDiveRolling) {
+            transform.position += transform.forward * speed * diveRollSpeedMultiplier * Time.deltaTime;
+            var pos = transform.position;
+            pos.y = 1f;
+            pos.y *= diveRollYPosMultiplier;
+            transform.position = pos;
+            Unrotate.position = transform.position;
+            Unrotate.rotation = Quaternion.identity;
+            return;
+        }
+        
         var h = Input.GetAxis("Horizontal");
         var v = Input.GetAxis("Vertical");
 
@@ -27,21 +76,25 @@ public class PlayerController : MonoBehaviour {
         if (movementVector.y < 0)
             yAngle = 360 - yAngle;
 
+        if (Input.GetButton("Fire2") && enableDiveRoll) {
+            DiveRollStart();
+        }
+
         if (h != 0 || v != 0) {
-            myAnimator.SetBool("isMoving", true);
+            isMoving = true;
+            myAnimator.SetBool("isMoving", isMoving);
             SmoothRotate(180 - yAngle - 45);
 
-            transform.position += transform.forward * speed * Time.deltaTime;
-        } else {
-            myAnimator.SetBool("isMoving", false);
-        }
+            if (isShooting) {
+                transform.position += transform.forward * speed * shootingMoveMultiplier * Time.deltaTime;
+            } else {
+                transform.position += transform.forward * speed * Time.deltaTime;
+            }
 
-        if (Input.GetButton("Fire1")) {
-            myAnimator.SetBool("isShooting", true);
         } else {
-            myAnimator.SetBool("isShooting", false);
+            isMoving = false;
+            myAnimator.SetBool("isMoving", isMoving);
         }
-
 
         Unrotate.position = transform.position;
         Unrotate.rotation = Quaternion.identity;
