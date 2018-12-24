d523e40420f33c94e628c3a6f965091b1cedda7b
IIzzaya <linxuan_0709@icloud.com>
2018-12-24 20:57:54 +0800

    加入角色碰撞和武器射击弹射

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,19 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EWeaponType {
+	Rifle,
+	Laser,
+	Missile
+}
+
 public class Weapon : MonoBehaviour {
 
 	public Transform muzzle;
-	public float cooldown;
 	public GameObject bulletPrefab;
+
+	[Header("Weapon Proerties 武器参数")]
+	[SerializeField]
+	public EWeaponType type;
+	public float coolDown;
+	private float coolDownTimer = 0;
 	public float bulletSpeed;
 	public float bulletLifeSpan;
+	public int bounceTimes;
 
 	public void Fire(float yAngle) {
-		Debug.Log(yAngle);
-		var bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.Euler(0, yAngle, 0));
-		var bulletInfo = bullet.GetComponent<Bullet>();
-		bulletInfo.speed = bulletSpeed;
-		Destroy(bullet, bulletLifeSpan);
+		switch (type) {
+
+			case EWeaponType.Rifle:
+				if (Time.time - coolDownTimer > coolDown) {
+					var bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.Euler(0, yAngle, 0));
+					var bulletInfo = bullet.GetComponent<Bullet>();
+					bulletInfo.speed = bulletSpeed;
+					bulletInfo.bounceLeftTimes = bounceTimes;
+					Destroy(bullet, bulletLifeSpan);
+					coolDownTimer = Time.time;
+				}
+				break;
+
+			case EWeaponType.Laser:
+
+				break;
+
+			case EWeaponType.Missile:
+
+				break;
+		}
+
 	}
 }
