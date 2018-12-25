1fea6ca4af35734b2678d587a0e70d7badd058a2
Dictatura <dictatura@yeah.net>
2018-12-25 23:28:20 +0800

    近战敌人、远程敌人
    近战敌人成，远程敌人的攻击检测还没有完成，敌人死亡的消融特效还没有完成

---
+++ Assets/Scripts/EnemyScript/LichAttack.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LichAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 10.0f;
+    public int attackDamage = 10;
+    public int scoreValue = 10;
+    public AudioClip attackClip;
+    AudioSource enemyAudio;
+
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
+        {
+            Attack();
+        }
+
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetBool("PlayerDead", true);
+        }
+    }
+
+
+    void Attack()
+    {
+        timer = 0f;
+
+        if (playerHealth.currentHealth > 0)
+        {
+            anim.SetTrigger("Attack");
+            //anim.ResetTrigger("Attack");
+            playerHealth.TakeDamage(attackDamage);
+            enemyAudio.clip = attackClip;
+            enemyAudio.Play();
+        }
+    }
+}
+
